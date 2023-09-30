d9c13b2a6527bf74619fe8dc5c794368a09b87c6
aanatyrnal <cacina_ang1891@mail.ru>
2023-09-30 17:52:47 +0300

    fix activity

--- dobriki/activity/models.py
+++ dobriki/activity/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Activity(models.Model):
     name = models.CharField(max_length=255, verbose_name="Наименование")
-    value = models.CharField(max_length=255, verbose_name="Значение")
+    coefficient = models.DecimalField(max_digits=3, decimal_places=1, verbose_name="Коэффициент", default=1.0)
     description = models.TextField(verbose_name="Описание", blank=True, null=True)
 
     def __str__(self):
