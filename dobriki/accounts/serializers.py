d4d01d08658356797c88153337b919be3a7b0b9c
Ilham Aliyev <idaliev@ozon.ru>
2023-09-30 13:46:07 +0300

    add infrastruscture

---
+++ dobriki/accounts/serializers.py
@@ -0,0 +1,49 @@
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+from competitions.models import UserTeam
+from competitions.serializers import UserTeamSerializer
+
+UserModel = get_user_model()
+
+
+class TeamSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserTeam
+        fields = '__all__'
+
+
+class UserDetailsSerializer(serializers.ModelSerializer):
+    """
+    User model w/o password
+    """
+    teams = serializers.SerializerMethodField(read_only=True)
+
+    @staticmethod
+    def validate_username(username):
+        if 'allauth.account' not in settings.INSTALLED_APPS:
+            # We don't need to call the all-auth
+            # username validator unless its installed
+            return username
+
+        from allauth.account.adapter import get_adapter
+        username = get_adapter().clean_username(username)
+        return username
+
+    class Meta:
+        extra_fields = []
+        if hasattr(UserModel, 'USERNAME_FIELD'):
+            extra_fields.append(UserModel.USERNAME_FIELD)
+        if hasattr(UserModel, 'EMAIL_FIELD'):
+            extra_fields.append(UserModel.EMAIL_FIELD)
+        if hasattr(UserModel, 'first_name'):
+            extra_fields.append('first_name')
+        if hasattr(UserModel, 'last_name'):
+            extra_fields.append('last_name')
+        model = UserModel
+        fields = ['id', *extra_fields, 'teams']
+        read_only_fields = ('email','date_joined')
+
+    def get_teams(self, obj):
+        return UserTeamSerializer(obj.teams, many=True).data
---
+++ dobriki/accounts/views.py
@@ -0,0 +1,30 @@
+from django.contrib.auth import get_user_model
+from rest_framework.generics import RetrieveUpdateAPIView
+from rest_framework.permissions import IsAuthenticated
+
+from accounts.serializers import UserDetailsSerializer
+
+
+class UserDetailAPIView(RetrieveUpdateAPIView):
+    """
+    Reads and updates UserModel fields
+    Accepts GET, PUT, PATCH methods.
+
+    Default accepted fields: username, first_name, last_name
+    Default display fields: pk, username, email, first_name, last_name
+    Read-only fields: pk, email
+
+    Returns UserModel fields.
+    """
+    serializer_class = UserDetailsSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
+
+    def get_queryset(self):
+        """
+        Adding this method since it is sometimes called when using
+        django-rest-swagger
+        """
+        return get_user_model().objects.none()
