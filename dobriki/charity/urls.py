086656f259b527f334785a6397d15dd8c1454f74
aanatyrnal <cacina_ang1891@mail.ru>
2023-10-01 01:39:54 +0300

    add transfer_money

--- dobriki/charity/urls.py
+++ dobriki/charity/urls.py
@@ -1,8 +1,17 @@
 from rest_framework.routers import DefaultRouter
-from charity.views import CharityViewSet, CharitySubscriptionViewSet
+from charity.views import CharityViewSet, transfer_money, CharitySubscriptionViewSet
+from django.urls import path, include
 
 router = DefaultRouter()
-router.register(r'charity', CharityViewSet)
-router.register(r'charity-subscription', CharitySubscriptionViewSet)
+subscription_router = DefaultRouter()
 
-urlpatterns = router.urls
+router.register(r'charity', CharityViewSet)
+subscription_router.register(r'charity-subscription', CharitySubscriptionViewSet)
+
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('', include(subscription_router.urls)),
+
+    path('transfer-money/', transfer_money, name='transfer-money'),
+]
--- dobriki/charity/views.py
+++ dobriki/charity/views.py
@@ -1,9 +1,10 @@
 from rest_framework import viewsets
 from rest_framework.decorators import action
+from rest_framework.decorators import api_view
+from rest_framework import status
 from rest_framework.response import Response
-from charity.models import Charity, CharitySubscription
+from charity.models import Charity, CharitySubscription, UserCharity
 from charity.serializers import CharitySerializer, CharitySubscriptionSerializer
-
 
 class CharityViewSet(viewsets.ModelViewSet):
     queryset = Charity.objects.all()
@@ -30,3 +31,29 @@ class CharitySubscriptionViewSet(viewsets.ModelViewSet):
         return Response({'result': "successfully applied"}, status=200)
 
 
+@api_view(['POST'])
+def transfer_money(request):
+    user = request.user
+    amount = request.data.get('amount')
+    charity_id = request.data.get('charity_id')
+
+    try:
+        user_charity = UserCharity.objects.get(user=user)
+    except UserCharity.DoesNotExist:
+        return Response({'error': 'User has no associated charity'}, status=status.HTTP_400_BAD_REQUEST)
+
+    if user_charity.sum < amount:
+        return Response({'error': 'Not enough funds'}, status=status.HTTP_400_BAD_REQUEST)
+
+    user_charity.sum -= amount
+    user_charity.save()
+
+    try:
+        charity = Charity.objects.get(pk=charity_id)
+    except Charity.DoesNotExist:
+        return Response({'error': 'Charity not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    charity.got_sum += amount
+    charity.save()
+
+    return Response({'message': 'Money transferred successfully'}, status=status.HTTP_200_OK)
