a057b8247a987114adb2639dfdd7e4c1480375f7
aanatyrnal <cacina_ang1891@mail.ru>
2023-09-30 13:32:02 +0300

    fix config

--- dobriki/config/views.py
+++ dobriki/config/views.py
@@ -2,10 +2,12 @@ from rest_framework import viewsets
 from rest_framework.filters import OrderingFilter, SearchFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Config
+from .serializers import ConfigSerializer
 
 class ConfigViewSet(viewsets.ModelViewSet):
     queryset = Config.objects.all()
-    filter_backends = [OrderingFilter, SearchFilter, DjangoFilterBackend]
+    # filter_backends = [OrderingFilter, SearchFilter, DjangoFilterBackend]
+    serializer_class = ConfigSerializer
     ordering_fields = ['name', 'coefficient']
     ordering = ['name']
     search_fields = ['name', 'description']
